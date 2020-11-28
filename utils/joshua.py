e32cec2dca2b1708560f8d5eb24a6b1d78205a1f
unsw-cse-soc <boualem.benatallah@gmail.com>
2020-11-28 18:46:42 +1100

    Update joshua.py

--- utils/joshua.py
+++ utils/joshua.py
@@ -1,7 +1,6 @@
 import requests
 from artemis.fileman.disk_memoize import memoize_to_disk
 
-
 @memoize_to_disk
 def joshua_paraphrase(sentence, paraphrase_count):
     data = requests.get("http://localhost:5674/translate?q=" + sentence)
